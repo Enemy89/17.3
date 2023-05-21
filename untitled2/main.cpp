786efbe9a69670e8b803f5224cb5f2aefa59fdb3
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2023-05-21 18:29:37 +0300

    Add files via upload

---
+++ untitled2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int str_lenght(const char* str) {
+    int i=0;
+    int count=0;
+    while (*(str+i)!='\0') {
+        ++i;
+        ++count;
+    }
+    return count;
+}
+
+bool substr (const char* ptext, const char* word) {
+    bool found=false;
+    for (int startPos=0; startPos+str_lenght(word)<= str_lenght(ptext); ++startPos){
+        bool match=true;
+        for (int i=0; i<str_lenght(word); ++i) {
+            if (*(word+i)!= *(ptext+(i+startPos))) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            found=true;
+            break;
+        }
+    }
+    if (found)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    const char* text = "Hello world";
+    const char* word="wor";
+
+    substr(text, word);
+}
